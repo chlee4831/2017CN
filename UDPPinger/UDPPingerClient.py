a2990ea2da519ae72f724bb15271e29b58c95efc
chlee_laptop <chlee4831@gmail.com>
2017-04-05 15:17:25 +0900

    first version

---
+++ UDPPinger/UDPPingerClient.py
@@ -0,0 +1,39 @@
+import time
+from socket import *
+
+# input IP address and message
+ip = raw_input("Enter IP address or \'localhost\': ")
+if ip == 'localhost':
+    ip = '127.0.0.1'
+message = raw_input("Enter message: ")
+
+serverAddr = (ip, 12000)
+pings = 1
+
+# Send ping 10 times
+while pings <= 10:
+
+    # Create a UDP socket
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    # Set a timeout value of 1 second
+    clientSocket.settimeout(1)
+
+    # Send ping
+    start = time.time()
+    clientSocket.sendto(message, serverAddr)
+
+    #If data is received back from server, print
+    try:
+        message, address = clientSocket.recvfrom(1024)
+        end = time.time()
+        rtt = end - start
+        print("Respond: " + message)
+        print("Pings: " + str(pings))
+        print("RTT: " + str(rtt*1000) + "ms\n")
+
+    #If data is not received back from server, print it has timed out
+    except timeout:
+        print("Requset timed out\n")
+
+    pings += 1
